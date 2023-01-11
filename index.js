c85e45dfb1ad9805a76bd4263a6f6828b243c7a9
Istvan Petrovan <petrovanistvan@gmail.com>
2023-01-11 13:00:40 +0100

    iteration-3 done

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-// Iteration 1: Names and Input
-
 const hacker1 = "Istvan";
-const hacker2 = "insert random name";
+const hacker2 = "Zsofia";
 console.log("The driver's name is " + hacker1);
 console.log("The navigator's name is " + hacker2);
 
-// Iteration 2: Conditionals
-
 if (hacker1.length > hacker2.length) {
-    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
-  } else if (hacker1.length < hacker2.length) {
-    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
-  } else {
-    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
-  }
-
-// Iteration 3: Loops
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else {
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
 
 let hacker1print = "";
 
@@ -32,3 +26,17 @@ for (let i = hacker2.length - 1; i >= 0; i--) {
 }
 
 console.log(hacker2print);
+
+function compare(hacker1, hacker2) {
+  if (hacker1 === hacker2) {
+    return "What?! You both have the same name?";
+  }
+
+  if (hacker1 > hacker2) {
+    return "Yo, the navigator goes first definitely.";
+  }
+
+  return "The driver's name goes first.";
+}
+
+console.log(compare(hacker1, hacker2));
